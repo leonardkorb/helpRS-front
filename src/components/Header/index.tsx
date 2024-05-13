c6e1fd6ec703242b0818085c71078de87542d8f0
Leonardo Korb <leonardo.korb-terceiro@cea.com.br>
2024-05-13 00:35:03 -0300

    Add menu mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 import Logo from '../../images/logo.png'
 
 const Header = () => {
+
+    const [menuMobile, setMenuMobile] = useState(false)
     let width = 0
     if (typeof window !== 'undefined') {
         // Your client-side code that uses window goes here
@@ -20,26 +23,55 @@ const Header = () => {
         <header>
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container >
-                    <Navbar.Brand href="/"><img style={{ width: 70 }} src={Logo}/> HELP RS</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="/list/missing">Desaparecidos</Nav.Link>
-                        <Nav.Link href="/list/missingpets">Pets Desaparecidos</Nav.Link>
-                        <Nav.Link href="/list/shelters">Abrigos</Nav.Link>
-                        <Nav.Link href="/list/donations">Donativos</Nav.Link>
-                        
-                        <Dropdown>
-                            <Dropdown.Toggle variant="success" id="dropdown-create">
-                                Cadastrar
-                            </Dropdown.Toggle>
+                    <Navbar.Brand href="/"><img style={{ width: 70 }} src={Logo} /> HELP RS</Navbar.Brand>
 
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="/create/missing">Pessoa Desaparecida</Dropdown.Item>
-                                <Dropdown.Item href="/create/missingpets">Animal Desaparecido</Dropdown.Item>
-                                <Dropdown.Item href="/create/shelters">Abrigo</Dropdown.Item>
-                                <Dropdown.Item href="/create/donations">Ajuda aos desabrigados</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </Nav>
+                    {width < 1024 ? <Button variant="light" onClick={() => setMenuMobile(true)}>Menu</Button> : (
+                        <Nav className="me-auto">
+                            <Nav.Link href="/list/missing">Desaparecidos</Nav.Link>
+                            <Nav.Link href="/list/missingpets">Pets Desaparecidos</Nav.Link>
+                            <Nav.Link href="/list/shelters">Abrigos</Nav.Link>
+                            <Nav.Link href="/list/donations">Donativos</Nav.Link>
+
+                            <Dropdown>
+                                <Dropdown.Toggle variant="success" id="dropdown-create">
+                                    Cadastrar
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="/create/missing">Pessoa Desaparecida</Dropdown.Item>
+                                    <Dropdown.Item href="/create/missingpets">Animal Desaparecido</Dropdown.Item>
+                                    <Dropdown.Item href="/create/shelters">Abrigo</Dropdown.Item>
+                                    <Dropdown.Item href="/create/donations">Ajuda aos desabrigados</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Nav>
+                    )}
+                    {menuMobile ? (
+                        <div className="menu-container">
+                            <div className="menu-mobile">
+                                <header>
+                                    <button onClick={() => setMenuMobile(false)}>x</button>
+                                </header>
+
+                                <section>
+                                    <h5>Listas</h5>
+                                    <Nav.Link href="/list/missing">Desaparecidos</Nav.Link>
+                                    <Nav.Link href="/list/missingpets">Pets Desaparecidos</Nav.Link>
+                                    <Nav.Link href="/list/shelters">Abrigos</Nav.Link>
+                                    <Nav.Link href="/list/donations">Donativos</Nav.Link>
+                                </section>
+                                <section>
+                                    <h5>Cadastre</h5>
+                                    <Nav.Link href="/create/missing">Pessoa Desaparecida</Nav.Link>
+                                    <Nav.Link href="/create/missingpets">Animal Desaparecido</Nav.Link>
+                                    <Nav.Link href="/create/shelters">Abrigo</Nav.Link>
+                                    <Nav.Link href="/create/donations">Ajuda aos desabrigados</Nav.Link>
+                                    
+                                </section>
+                            </div>
+                        </div>
+
+                    ) : null}
                 </Container>
             </Navbar>
         </header>
